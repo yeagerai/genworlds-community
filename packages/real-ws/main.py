1d219cc7a7cd40a01682d7cfe83ac3cd4e6c8833
jmlago <josemlagoalonso@gmail.com>
2023-07-03 17:26:44 +0100

    genworlds 0.0.4 and real-ws hooked up

--- packages/real-ws/main.py
+++ packages/real-ws/main.py
@@ -1,4 +1,4 @@
-from genworlds.sockets.world_socket_server import start_from_command_line
+from genworlds.sockets.world_socket_server import start
 
 if __name__ == "__main__":
-    start_from_command_line()
+    start(host = "0.0.0.0", port = 7456)
