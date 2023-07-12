0cf0a9a774e7db349a14d9f203d9b3ee9fb6a6ba
jmlago <josemlagoalonso@gmail.com>
2023-07-12 09:01:12 +0100

    random map

--- packages/16bit-front/packages/common-types/src/enums/maps.ts
+++ packages/16bit-front/packages/common-types/src/enums/maps.ts
@@ -6,13 +6,14 @@ export enum MAPS {
   TEST_LABORATORY = 'test-laborator',
   LIBRARY = 'test',
   TEST_LIBRARY = 'test-library',
+  RANDOM = 'random',
 }
 
 interface MapInfo {
   tilemap: string;
 }
 
-export const AVAILABLE_MAPS: { [key in MAPS]: MapInfo } = {
+export const AVAILABLE_MAPS: Partial<{ [key in MAPS]: MapInfo | (() => MapInfo) }> = {
   [MAPS.TEST_MAP]: {
     tilemap: 'Mapa_Prova_Anna_02.json',
   },
@@ -34,7 +35,10 @@ export const AVAILABLE_MAPS: { [key in MAPS]: MapInfo } = {
   [MAPS.TEST_LIBRARY]: {
     tilemap: 'MAPA BIBLIOTECA.json',
   },
-  // [MAPS.BIBLIOTECA]: {
-  //   tilemap: 'Mapa Biblioteca.json',
-  // },
+};
+
+AVAILABLE_MAPS[MAPS.RANDOM] = function() {
+  const mapKeys = Object.keys(AVAILABLE_MAPS).filter(key => key !== MAPS.RANDOM) as Exclude<MAPS, MAPS.RANDOM>[];
+  const randomKey = mapKeys[Math.floor(Math.random() * mapKeys.length)];
+  return AVAILABLE_MAPS[randomKey] as MapInfo;
 };
