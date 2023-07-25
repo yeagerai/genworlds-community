d80ad71a3c6f2adde023415963e5b6126ccf9eab
jmlago <josemlagoalonso@gmail.com>
2023-07-25 12:24:28 +0100

    title and favicon

--- packages/nano-vue/vue.config.js
+++ packages/nano-vue/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js', //entry for the public page
+      template: 'public/index.html', // source template
+      filename: 'index.html', // output as dist/*
+      title: 'GenWorlds Community', // when using template and/or filename options, you may need to specify the title in vue.config.js
+    }
+  }
 })
